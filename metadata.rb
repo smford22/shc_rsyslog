30c1d99b011fab2ca0e7a7a36e487ed5043aa1e8
Scott Ford <smford22@gmail.com>
2017-03-07 14:54:45 -0800

    initial cookbook of rsyslog wrapper

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ license 'all_rights'
 description 'Installs/Configures shc_rsyslog'
 long_description 'Installs/Configures shc_rsyslog'
 version '0.1.0'
+depends 'rsyslog'
+
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,6 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+node.default['rsyslog']['server_ip'] = '10.252.241.107'
+node.default['rsyslog']['port'] = '514'
+include_recipe 'rsyslog::client'
