d670dd53052124c902d7b5fe31b10553dcfc7067
Scott Ford <smford22@gmail.com>
2017-03-07 14:38:58 -0800

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# shc_rsyslog
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: shc_rsyslog
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
